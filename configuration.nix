0582ba6a082878b0fbf0b49741b051da48af8a88
Kshtij Sharma <kshitij.dev@proton.me>
2026-06-16 22:46:40 +0530

    init minimal nixos with hyprland

---
+++ configuration.nix
@@ -0,0 +1,67 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  # Boot Manager
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "horizon"; 
+  networking.networkmanager.enable = true;
+
+  # Experimental Features
+  nix.settings.experimental-features= [ "nix-command" "flakes" ];
+
+  # Locale
+  time.timeZone = "Asia/Kolkata";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  
+  # System Packages
+  programs.zsh.enable = true;
+  programs.hyprland.enable = true;
+  environment.systemPackages = with pkgs; [
+    vim 
+  ];
+
+  # Users 
+  users.users.conart = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; 
+    shell = pkgs.zsh;
+    packages = [];
+  };
+
+  # SUID Wrapper
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # Services
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Are you absolutely sure you need to change this variable?
+  system.stateVersion = "26.05"; 
+}
+
---
+++ home.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, ... }:
+
+{
+  # Replace with your exact username and home directory path
+  home.username = "conart";
+  home.homeDirectory = "/home/conart";
+
+  # Do not change this value unless you thoroughly read the release notes.
+  home.stateVersion = "26.05"; 
+
+  # User-specific packages you want installed
+  home.packages = with pkgs; [
+    firefox
+    kitty
+    hyprlauncher
+    neovim
+  ];
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Kshitij Sharma";
+	email = "kshitij.dev@proton.me";
+      };
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  
+    # These are the modern Zsh features that make it great
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -la";
+      update = "sudo nixos-rebuild switch --flake ~/.dotfiles/#horizon";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {};
+    
+    extraConfig = ''
+local mod = "SUPER"
+local terminal = "kitty"
+local launcher = "hyprlauncher"
+
+hl.config({
+input = {
+  kb_layout = "us",
+  kb_options = "caps:escape"
+},
+})
+
+hl.bind(mod .. " + Q", hl.dsp.exec_cmd(terminal))
+hl.bind(mod .. " + C", hl.dsp.window.close())
+hl.bind(mod .. " + M", hl.dsp.exit())
+hl.bind(mod .. " + SPACE", hl.dsp.exec_cmd(launcher))
+    '';
+  };
+
+  # Let Home Manager install and manage itself
+  programs.home-manager.enable = true;
+}
