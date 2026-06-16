6e778c5132aeb1a162e2d72ee3f87e1c0d4f4b5b
Kshitij Sharma <kshitij.dev@proton.me>
2026-06-16 23:50:55 +0530

    Add git aliases, and some config

--- home.nix
+++ home.nix
@@ -27,6 +27,21 @@
         name = "Kshitij Sharma";
 	email = "kshitij.dev@proton.me";
       };
+      alias = {
+        st = "status";
+        lg = "log -n 10 --oneline";
+        ci = "commit";
+        co = "checkout";
+        br = "branch";
+        fk = "commit --amend --no-edit";
+        rb = "rebase";
+        rbc = "rebase --continue";
+      };
+
+      core.editor = "nvim";
+      init.defaultBranch = "master";
+      pull.rebase = true;
+      "url \"git@github.com:\"".insteadOf = "https://github.com/";
     };
   };
 
