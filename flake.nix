9ca3674b088010ba8877c0921db96b8ee5e00082
Kshitij Sharma <kshitij.dev@proton.me>
2026-06-16 22:46:40 +0530

    init minimal nixos with hyprland

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "System configuration flake";
+
+  inputs = {
+    # Stable or unstable nixpkgs core
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    # Home manager framework
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      horizon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            
+            home-manager.users.conart = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
