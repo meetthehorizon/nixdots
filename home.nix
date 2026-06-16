63fa8dfb42bb5305b84f2afa7f41c3104ba2edfc
Kshtij Sharma <kshitij.dev@proton.me>
2026-06-16 23:42:38 +0530

    Add starship (Pure) prompt

--- home.nix
+++ home.nix
@@ -51,6 +51,62 @@
     };
   };
 
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      format = "$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
+
+      directory = {
+        style = "blue";
+      };
+
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
+      };
+
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        # These preserve the zero-width spaces from your original config
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+
+      git_state = {
+        # Backslashes must be escaped in Nix strings
+        format = "\\([$state( $progress_current/$progress_total)]($style)\\) ";
+        style = "bright-black";
+      };
+
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "yellow";
+      };
+
+      python = {
+        format = "[$virtualenv]($style) ";
+        style = "bright-black";
+        # Empty arrays map perfectly to Nix lists
+        detect_extensions = [ ];
+        detect_files = [ ];
+      };
+    };
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {};
