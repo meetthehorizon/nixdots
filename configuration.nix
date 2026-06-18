941e57ebbd25b2ea7359ef97e5558394a60c3589
Kshitij Sharma <kshitij.dev@proton.me>
2026-06-19 04:16:59 +0530

    Add limit on number of generations to persist

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
   # Boot Manager
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 7;
 
   # Networking
   networking.hostName = "horizon";
