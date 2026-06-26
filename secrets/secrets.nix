9257dc93f505d2dcddcbd855038214619fb7650f
Kshitij Sharma <kshitij.dev@proton.me>
2026-06-27 03:57:07 +0530

    feat(bootstrap): automate GitHub credential discovery and SSH key registration

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 # Auto-generated secrets configuration. Do not edit manually.
 let
-  userKey = "age1fwrtqyn9cxcsmnsydq8v36frsjnk5gk8aclwun5nnc8hfzjm5sas4tlcm0";
+  userKey = "age1un9tnzc4gvwwq64449vkwkk08kzk7363k8ru2p9p8nxlna23699sm4zunl";
   hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILS9YISdkGd7QMXHjKXlLpdSCRXKzTg79W9NKtAT9vHO root@horizon";
   keys = if hostKey != "" then [ userKey hostKey ] else [ userKey ];
 in
