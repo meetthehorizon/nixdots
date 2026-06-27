8332a15a5f76f67e002d8ca5d448039f4108c350
Kshtij Sharma <kshitij.dev@proton.me>
2026-06-27 14:02:54 +0530

    Remove Python TUI

--- modules/files.nix
+++ modules/files.nix
@@ -12,7 +12,5 @@ in {
     ".face".source = assets.userIcon;
     ".config/fastfetch/logo.png".source = assets.nixosIcon;
     ".gemini/GEMINI.md".source = ../GEMINI-global.md;
-
-    ".local/bin/nixdots".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/scripts/nixdots";
   };
 }
