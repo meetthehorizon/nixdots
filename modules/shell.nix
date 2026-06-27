42b3307b3e14063f3482ac6e53f6e869c2393a14
Kshitij Sharma <kshitij.dev@proton.me>
2026-06-27 11:49:26 +0530

    cleanup: remove ghost zsh configurations and scripts/asix

--- modules/shell.nix
+++ modules/shell.nix
@@ -2,45 +2,6 @@
   config,
   ...
 }: {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-
-    # These are the modern Zsh features that make it great
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    defaultKeymap = "viins";
-
-    history = {
-      size = 10000;
-      save = 10000;
-      expireDuplicatesFirst = true;
-      ignoreDups = true;
-      ignoreSpace = true;
-      share = true;
-    };
-
-    shellAliases = {
-      ls = "eza --icons";
-      ll = "eza -la --icons --group-directories-first";
-      v = "nvim";
-      g = "git";
-      clr = "clear && fastfetch";
-
-      nean = "sudo nix-collect-garbage -d";
-      nate = "sudo nixos-rebuild switch --flake ~/nixdots/#horizon";
-      nest = "sudo nixos-rebuild test --flake ~/nixdots/#horizon";
-    };
-
-    initContent = ''
-      fastfetch
-      if [ -f /home/conart/.config/gh/github-pat ]; then
-        export GITHUB_TOKEN=$(cat /home/conart/.config/gh/github-pat)
-        export GH_TOKEN=$GITHUB_TOKEN
-      fi
-    '';
-  };
 
   programs.fish = {
     enable = true;
@@ -148,7 +109,6 @@
 
   programs.starship = {
     enable = true;
-    enableZshIntegration = true;
     enableFishIntegration = true;
 
     settings = {
@@ -223,19 +183,16 @@
 
   programs.zoxide = {
     enable = true;
-    enableZshIntegration = true;
     enableFishIntegration = true;
   };
 
   programs.fzf = {
     enable = true;
-    enableZshIntegration = true;
     enableFishIntegration = true;
   };
 
   programs.eza = {
     enable = true;
-    enableZshIntegration = true;
     enableFishIntegration = true;
     git = true;
     icons = "auto";
