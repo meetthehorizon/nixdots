039dbdcbe4e479a76be6119fa011fc1b1e325cea
Kshtij Sharma <kshitij.dev@proton.me>
2026-07-05 00:57:08 +0530

    feat(nixvim): show hidden files in oil.nvim

--- modules/nixvim/plugins/oil.nix
+++ modules/nixvim/plugins/oil.nix
@@ -6,6 +6,7 @@
         default_file_explorer = true;
         delete_to_trash = true;
         skip_confirm_for_simple_edits = true;
+        view_options.show_hidden = true;
       };
     };
   };
