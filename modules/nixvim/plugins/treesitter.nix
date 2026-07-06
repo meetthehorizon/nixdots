d6c5635e402718deae7c5d5f002cc102e63d627f
Kshitij Sharma <kshitij.dev@proton.me>
2026-07-07 00:12:39 +0530

    feat: configure treesitter grammars

--- modules/nixvim/plugins/treesitter.nix
+++ modules/nixvim/plugins/treesitter.nix
@@ -1,5 +1,28 @@
-{...}: {
-  programs.nixvim = {
-    plugins.treesitter.enable = true;
+{config, ...}: {
+  programs.nixvim.plugins.treesitter = {
+    enable = true;
+
+    highlight.enable = true;
+    indent.enable = true;
+
+    grammarPackages = with config.programs.nixvim.plugins.treesitter.package.builtGrammars; [
+      bash
+      c
+      cpp
+      css
+      go
+      html
+      javascript
+      json
+      lua
+      markdown
+      nix
+      python
+      rust
+      svelte
+      toml
+      typescript
+      yaml
+    ];
   };
 }
