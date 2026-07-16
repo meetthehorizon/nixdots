60064a948d072ceff6723b897c43344692f06076
Kshitij Sharma <kshitij.dev@proton.me>
2026-07-16 19:42:21 +0530

    fix: revert rofi to default theme

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -4,6 +4,5 @@
   programs.rofi = {
     enable = true;
     font = "${config.font.mono} ${toString config.font.size.base}";
-    theme = "${config.xdg.configHome}/rofi/themes/custom.rasi";
   };
 }
