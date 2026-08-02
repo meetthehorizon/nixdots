9afaa2140ec11fe4ab3a6646310609618042b146
Kshitij Sharma <kshitij.dev@proton.me>
2026-08-03 02:33:38 +0530

    feat: update init applications

--- modules/hyprland/config.nix
+++ modules/hyprland/config.nix
@@ -99,10 +99,12 @@ in {
       hl.on("hyprland.start", function()
         hl.exec_cmd("dbus-update-activation-environment --systemd --all")
         hl.exec_cmd("hyprctl setcursor ${config.cursorTheme} 24")
-        hl.exec_cmd(terminal .. " -e nvim", { workspace = "1 silent" })
+
+        hl.exec_cmd(terminal, { workspace = "1 silent" })
         hl.exec_cmd(browser, { workspace = "2 silent" })
         hl.exec_cmd(terminal, { workspace = "3 silent" })
-        hl.exec_cmd("footclient spotatui", { workspace = "4 silent" })
+        hl.exec_cmd(anki, { workspace = "9 silent" })
+        hl.exec_cmd(obsidian, { workspace = "10 silent" })
         hl.dispatch(hl.dsp.focus({ workspace = "3" }))
       end)
     '';
