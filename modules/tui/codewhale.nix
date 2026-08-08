38d1b865b08b987336bcb108906a5ab390bf8fe2
Kshitij Sharma <kshitij.dev@proton.me>
2026-08-08 13:32:32 +0530

    feat: bump codewhale version

--- modules/tui/codewhale.nix
+++ modules/tui/codewhale.nix
@@ -1,19 +1,19 @@
 {pkgs, ...}: let
-  version = "0.8.67";
+  version = "0.9.4";
 
   codewBin = pkgs.fetchurl {
     url = "https://github.com/Hmbown/CodeWhale/releases/download/v${version}/codew-linux-x64";
-    hash = "sha256-fFK++mV/jlUxlSgS7rFbtfdYZG4laUi0Kf73l6laK5M=";
+    hash = "sha256-mLsMUEqs/jkfHb9FsoyhU5hBPulKtEZB9G3A1TC4ogQ=";
   };
 
   codewhaleBin = pkgs.fetchurl {
     url = "https://github.com/Hmbown/CodeWhale/releases/download/v${version}/codewhale-linux-x64";
-    hash = "sha256-xl02Q6a1/+XI+YdfHDDfgZl2W2TLna9muDg9Rdn8q0s=";
+    hash = "sha256-0WVMZ030Cx8UUWo9v4Er90Pri60nBPIE5PA0aWwRXK0=";
   };
 
   codewhaleTuiBin = pkgs.fetchurl {
     url = "https://github.com/Hmbown/CodeWhale/releases/download/v${version}/codewhale-tui-linux-x64";
-    hash = "sha256-JkUeBFM/dennS6l0pi7mgbPQA3eTOUcYDhEydOGXBBs=";
+    hash = "sha256-yJRnSAKVCUestzW1uH56bhw2FZX9tWnPZ2+IhcnYdJQ=";
   };
 in {
   home.packages = [
